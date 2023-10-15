7871f27e2ea3e6043c1f2e76dc03a07b8a092f9e
Joerg Jungermann <joerg.jungermann@googlemail.com>
2023-10-16 00:00:05 +0200

    r1cbu-v0.18.0-vanilla: initial add

---
+++ r1cbu-v0.18.0-vanilla.Dockerfile.d/000_ensure_rootfs_in_target.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+# (C) 2023 Joerg Jungermann, GPLv2 see LICENSE
+
+PS4='> ${0##*/}: '
+set -x
+
+mv /target /target.old ;\
+mv /target.old/part2 /target ;\
+cp -a /target.old/part1/* /target/boot ;\
+cp -a /target.old/part3/* /target/mnt ;\
+rm -rf /target.old ;\
